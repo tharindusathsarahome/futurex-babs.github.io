d02a2555b0b900b3ee3498564d84bb017b2bb498
Tharindu Sathsara <tharindusathsarahome@gmail.com>
2023-08-26 10:25:26 +0530

    SiteAdded

---
+++ wp-admin/admin-ajax7bd9.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from wpdemo.archiwp.com/engitech/wp-admin/admin-ajax.php?action=process_simple_like&post_id=1263&nonce=1a519f634b&is_comment=0&disabled=true by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 09 Mar 2022 02:18:41 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=../plan-your-project-with-your-software/index.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="../plan-your-project-with-your-software/index.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from wpdemo.archiwp.com/engitech/wp-admin/admin-ajax.php?action=process_simple_like&post_id=1263&nonce=1a519f634b&is_comment=0&disabled=true by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 09 Mar 2022 02:18:41 GMT -->
+</HTML>
